fd94c67b1273d5fda5e655b6da79b2c8e01caf82
JosueAndradix <124620062+JosueAndradix@users.noreply.github.com>
2023-07-24 19:02:56 -0600

    pintar cards

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar = () => {
   return (
     <nav className='navbar bg-body-tertiary'>
       <div className='container-fluid'>
-        <a className='navbar-brand'>Navbar</a>
+        <a className='navbar-brand'>Series Perronas</a>
         <form className='d-flex' role='search'>
           <input className='form-control me-2' type='search' placeholder='Search' aria-label='Search' />
-          <button className='btn btn-outline-success' type='submit'>Search</button>
+          <button className='btn btn-outline-success' type='submit'>Buscar</button>
         </form>
       </div>
     </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Home = () => {
+  const [series, setSeries] = useState([])
+  useEffect(() => {
+    fetch('https://api.tvmaze.com/search/shows?q=popular')
+      .then(res => res.json())
+      .then(data => setSeries(data))
+      .catch(error => console.error(error))
+  },
+  [])
+
   return (
-    <h1>Home</h1>
+    <>
+      <div className='container'>
+        <h1 className='mb-4'>Home</h1>
+        <div className='row row-cols-1 row-cols-md-3 g-4'>
+          {series.map((seriesItem) => (
+            <div className='col' key={seriesItem.show.id}>
+              <div className='card h-100'>
+                <img
+                  src={seriesItem.show.image?.medium || '/placeholder-image.png'}
+                  alt={seriesItem.show.name}
+                  className='card-img-top'
+                />
+                <div className='card-body'>
+                  <h5 className='card-title'>{seriesItem.show.name}</h5>
+                  <p className='card-text'>{seriesItem.show.summary}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+    </>
+
   )
 }
 
